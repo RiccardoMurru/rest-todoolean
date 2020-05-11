27f68f4a5d6a932ab55b078936531d4f4fa36079
Riccardo Murru <riccardo.mr@gmail.com>
2020-05-11 15:00:06 +0200

    add. files

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Creazione di una todo list con le seguenti funzionalità, attraverso l’uso delle API, AJAX, jQuery e Handlebars
+ * Lettura di tutti i todo
+ * Creazione nuovo todo
+ * Cancellazione todo
+ */
+
+$(document).ready(function () {
+
+}); // end doc ready
